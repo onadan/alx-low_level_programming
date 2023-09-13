a136a4596fc7a6d1584ed4bf3b021457059fcb55
onadan <onadan@gmail.com>
2023-09-12 17:27:13 -0700

    task done

---
+++ 0x02-functions_nested_loops/2-alphabet_x10.c
@@ -0,0 +1,14 @@
+print_alphabet_x10(void)
+	int i;
+	char al;
+
+	for (i = 0; i < 11; i++)
+	for (i = 0; i < 10; i++)
+	{
+		for (al = 'a'; al <= 'z'; al++)
+		{
+			_putchar(al);
+		}
+		_putchar('\n');
+	}
+}
